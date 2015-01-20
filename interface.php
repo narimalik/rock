a18429c141545344b0e4520e6760d6e51f76c941
narimalik <malikanwaar@gmail.com>
2015-01-20 12:50:52 +0000

    Create interface.php

---
+++ interface.php
@@ -0,0 +1,12 @@
+<?php
+
+interface BandInterface{
+	public function addBand();
+	public function getBandById($id);
+	/*public function nextConcert();
+	public function getSongs($bandid);
+	public function getAlbum($bandid);*/
+}
+
+
+?>
